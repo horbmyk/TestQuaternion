42d1a8a49c8c27524f91692331f1b621c15aa753
horbmyk <67599881+horbmyk@users.noreply.github.com>
2021-09-28 10:42:44 +0300

    Task Ray Fixed

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -2,11 +2,39 @@ using UnityEngine;
 
 public class RayController : MonoBehaviour
 {
-    [SerializeField] GameObject StartObj;
-    [SerializeField] GameObject FinishObj;
-    private void FixedUpdate()
+    [SerializeField] private Transform _startPosRay;
+    [SerializeField] private Transform _targetPosRay;
+    private Ray _rayTargets;
+    private Ray _rayMouse;
+    private RaycastHit _hitTargets;
+    private RaycastHit _hitMouse;
+
+    private void Update()
     {
-        Ray ray = new Ray(StartObj.transform.position, FinishObj.transform.position);
-        Debug.DrawRay(gameObject.transform.position, FinishObj.transform.position, Color.green);
+        if (Input.GetMouseButton(0))
+            ChangeTarget();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            ShowInfoTarget();
+
+        Debug.DrawRay(_startPosRay.transform.position, _targetPosRay.position, Color.green);
+
+    }
+
+    private void ChangeTarget()
+    {
+        _rayMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_rayMouse, out _hitMouse) && _hitMouse.collider.gameObject)
+        {
+            _targetPosRay = _hitMouse.collider.gameObject.transform;
+            _rayTargets = new Ray(_startPosRay.position, _targetPosRay.position);
+        }
+    }
+
+    private void ShowInfoTarget()
+    {
+        if (Physics.Raycast(_rayTargets, out _hitTargets))
+            Debug.Log("Targets Name: " + _hitTargets.collider.name + "  " + " Distance to Target = " + _hitTargets.distance);
     }
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,8 @@ public class Test : MonoBehaviour
     private float z;
     private Quaternion RotationY = Quaternion.AngleAxis(7.2f, Vector3.up);
 
-
+    //Cделать через  Update
+    //«м≥нна V в ≥нспектор≥ 
     private void FixedUpdate()
     {
         transform.rotation *= RotationY;
