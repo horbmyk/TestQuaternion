e564bdc3f00dec5485012e6c0358e1980582a9b8
horbmyk <67599881+horbmyk@users.noreply.github.com>
2021-10-09 19:38:28 +0300

    Fixed

--- Assets/Scripts/WebRequest.cs
+++ Assets/Scripts/WebRequest.cs
@@ -6,17 +6,23 @@ namespace Test
 {
     public class WebRequest : MonoBehaviour
     {
+        public delegate void ShowJsonEventHandler(string json);
 
         void Start()
         {
-            StartCoroutine(GetText());
+            EventHandler instance = new EventHandler();
+            instance.EventShowJson += instance.ViewJsonFirst;
+            instance.EventShowJson += instance.ViewJsonSecond;
+            StartCoroutine(GetText(instance));
             // пост Html
             // 1 postman
             // 2 сделать post
             // 3 гет передавать с параметрами
             // Код ревью Microsoft
         }
-        private IEnumerator GetText()
+
+
+        private IEnumerator GetText(EventHandler eventHandler)
         {
             UnityWebRequest www = UnityWebRequest.Get("https://www.postman-echo.com/get");
             yield return www.SendWebRequest();
@@ -28,7 +34,31 @@ namespace Test
             {
                 string json = www.downloadHandler.text;
                 var desirializetGetData = JsonUtility.FromJson<ExampleJson>(json);
+                eventHandler.InvokeEvent(desirializetGetData.url);
             }
+        }
+
+        public class EventHandler
+        {
+            public event ShowJsonEventHandler EventShowJson = null;
+
+            public void InvokeEvent(string json)
+            {
+                EventShowJson.Invoke(json);
+            }
+
+            public void ViewJsonFirst(string json)
+            {
+                Debug.Log("ViewJsonFirst");
+                Debug.Log(json);
+            }
+
+            public void ViewJsonSecond(string json)
+            {
+                Debug.Log("ViewJsonSecond");
+                Debug.Log(json);
+            }
+
         }
     }
 }
